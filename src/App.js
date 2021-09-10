30efb38b47a0c515be6b0c390ed0d7f99d7cec85
Verson-tech <77786047+Verson-tech@users.noreply.github.com>
2021-09-10 15:28:40 +0300

    added navbar from bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
+  
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="https://images.unsplash.com/photo-1611273426858-450d8e3c9fce?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FyYm9uJTIwZGlveGlkZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://co2ding-2021.herokuapp.com/api-docs/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Let's get started!!!
-        </a>
-      </header>
+  return(
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Home Page</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarText">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">All Data</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Deposit</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Withdraw</a>
+        </li>
+      </ul>
+      <span className="navbar-text">
+      Create Account
+      </span>
     </div>
+  </div>
+</nav>
   );
 }
 
