77429619223f19403d4d3fe493c2feda7e0ebe17
Verson-tech <77786047+Verson-tech@users.noreply.github.com>
2021-09-10 14:54:52 +0300

    1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://images.unsplash.com/photo-1611273426858-450d8e3c9fce?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FyYm9uJTIwZGlveGlkZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
